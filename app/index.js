6bd42d81152973f62db35321f4b04f59547ea8f9
Chinatu Enyinnaya <chinatudev@gmail.com>
2020-08-20 00:10:43 +0100

    Retrying incomplte upload

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require('dotenv').config();
 
 const express = require('express');
@@ -7,3 +8,14 @@ const SERVER_PORT = 4000;
 app.use(express.static('public'));
 
 app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
+=======
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const SERVER_PORT = 4000;
+
+app.use(express.static('public'));
+
+app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
+>>>>>>> 08c3add73cafabe554a49aedbbaa53e66da92d90
