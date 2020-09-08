77e39c2543d4ee31552df0fd89618ec40d7aed04
Adekunle Adedayo <adekunleadedayo@Adekunles-MBP.fios-router.home>
2020-09-08 10:06:00 -0400

    Fix incorrect test case with deletion

--- app/test.js
+++ app/test.js
@@ -26,8 +26,8 @@ assert(users.getByMatricNumber(john.matricNumber).getFullName() === 'john doe');
 assert(users.authenticate(john.email, john.password) === true);
 assert(users.update({ firstname: 'jane' }, john.id) === true);
 assert(users.getById(john.id).getFullName() === 'jane doe', `user detail is not being updated by the update method`)
-assert(users.delete(jerry.id) === true);
-assert(users.getById(jerry.id) === null, `deleted user should no longer be avaiable in the data array. The getById method should now return null for the specified id`)
+assert(users.delete(jones.id) === true);
+assert(users.getById(jones.id) === null, `deleted user should no longer be avaiable in the data array. The getById method should now return null for the specified id`)
 assert(users.getAll().length === 1);
 
 
