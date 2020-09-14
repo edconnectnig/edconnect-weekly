978fcfc0c8594927992ae343a8ebda7c981732f9
Tola Anjorin <tola.anjorin@gmail.com>
2020-09-14 08:51:59 -0400

    updated starter for week6

--- app/server/routes/api.js
+++ app/server/routes/api.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const api = express.Router();
-const fs = require('fs');
-const path = require('path');
-const User = require('../models/users').User;
-const Users = require('../models/users').Users;
-const Project = require('../models/projects').Project;
-const Projects = require('../models/projects').Projects;
+const fs = require("fs");
+const path = require("path");
+const User = require("../models/users").User;
+const Users = require("../models/users").Users;
+const Project = require("../models/projects").Project;
+const Projects = require("../models/projects").Projects;
 
-const usersFile = path.join(__dirname, '../users.json');
-const projectsFile = path.join(__dirname, '../projects.json');
+const usersFile = path.join(__dirname, "../users.json");
+const projectsFile = path.join(__dirname, "../projects.json");
 
 const getFileAsJson = (file) => {
-    return JSON.parse(fs.readFileSync(file));
-}
+  return JSON.parse(fs.readFileSync(file));
+};
 
 const saveJsonFile = (file, data) => {
-    fs.writeFileSync(file, JSON.stringify({ data }));
-}
+  fs.writeFileSync(file, JSON.stringify({ data }));
+};
 
 const saveUsersDb = (data) => saveJsonFile(usersFile, data);
 const saveProjectsDb = (data) => saveJsonFile(projectsFile, data);
@@ -30,147 +30,119 @@ projects.data = getFileAsJson(projectsFile).data;
 const id = () => Math.random().toString(36).substring(2);
 
 const getCookie = (req, name) => {
-    const cookie =  req.headers['cookie'] || "";
-    for (const ck of cookie.split(';')) {
-        const vals = ck.split("=")
-        if (vals[0].trim() === name) {
-            return vals[1]
-        }
+  const cookie = req.headers["cookie"] || "";
+  for (const ck of cookie.split(";")) {
+    const vals = ck.split("=");
+    if (vals[0].trim() === name) {
+      return vals[1];
     }
-}
-const handlePost = (success, data,  errors, res) => {  
-    if (success) {
-        res.status(200).json({ status: 'ok', data });
-    } else {
-        res.status(400).json({ status: 'error', errors });
-    }
-}
+  }
+};
+const handlePost = (success, data, errors, res) => {
+  if (success) {
+    res.status(200).json({ status: "ok", data });
+  } else {
+    res.status(400).json({ status: "error", errors });
+  }
+};
 
 const requireLogin = (req, res, next) => {
+  const uid = getCookie(req, "uid");
+  if (uid) {
+    req.session.uid = uid;
+    next();
+  } else {
+    res.status(401).json({ status: "error", errors: "Unauthorized Access" });
+  }
+};
 
-   const uid = getCookie(req, 'uid');
-    if (uid) {
-        req.session.uid = uid;
-        next();
-    } else {
-        res.status(401).json({ status: 'error', errors: 'Unauthorized Access' });
-    }
-}
+api.post("/register", (req, res) => {
+  const {
+    firstname,
+    lastname,
+    email,
+    password,
+    matricNumber,
+    program,
+    graduationYear,
+  } = req.body;
 
-api.post('/register', (req, res) => {
-    const {
-        firstname,
-        lastname,
-        email,
-        password,
-        matricNumber,
-        program,
-        graduationYear
-    } = req.body;
-
-    const user = new User(id(), firstname, lastname, email, password, matricNumber, program, graduationYear);
-    const success = users.save(user);
-    if (success) {
-        saveUsersDb(users.data);
-    }
-    handlePost(success, {id: user.id} , users.errors, res);
+  const user = new User(
+    id(),
+    firstname,
+    lastname,
+    email,
+    password,
+    matricNumber,
+    program,
+    graduationYear
+  );
+  const success = users.save(user);
+  if (success) {
+    saveUsersDb(users.data);
+  }
+  handlePost(success, { id: user.id }, users.errors, res);
 });
 
-api.post('/login', (req, res) => {
-    const {
-        email,
-        password
-    } = req.body;
+api.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
-    const success = users.authenticate(email, password);
-    if (success) {
-        req.session.user = users.getByEmail(email);
-    }
-    handlePost(success,req.session.user,  users.errors, res);
+  const success = users.authenticate(email, password);
+  if (success) {
+    req.session.user = users.getByEmail(email);
+  }
+  handlePost(success, req.session.user, users.errors, res);
 });
 
-api.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.status(200).json({ status: 'ok' });
+api.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.status(200).json({ status: "ok" });
 });
 
-api.get('/loggedin', (req, res) => {
-    res.json(req.session.user);
+api.get("/users", (req, res) => {
+  res.json(users.getAll());
 });
 
-api.get('/users', (req, res) => {
-    res.json(users.getAll());
+api.get("/users/:id", (req, res) => {
+  res.json(users.getById(req.params.id));
 });
 
-api.get('/users/:id', (req, res) => {
-    res.json(users.getById(req.params.id));
+api.get("/projects", (req, res) => {
+  res.json(projects.getAll());
 });
 
-api.post('/users/update', (req, res) => {
-    const {
-        id,
-        obj
-    } = req.body;
-
-    const success = users.update(obj, id);
-    handlePost(success, users.errors, res);
+api.get("/projects/:id", (req, res) => {
+  res.json(projects.getById(req.params.id));
 });
 
-api.get('/projects', (req, res) => {
-    res.json(projects.getAll());
+api.post("/projects", requireLogin, (req, res) => {
+  const { name, abstract, authors, tags } = req.body;
+
+  const project = new Project(
+    id(),
+    name,
+    abstract,
+    authors,
+    tags,
+    req.session.uid
+  );
+  const success = projects.save(project);
+  if (success) {
+    saveProjectsDb(projects.data);
+  }
+  handlePost(success, {}, projects.errors, res);
 });
 
-api.get('/projects/showcase', (req, res) => {
-    res.json(projects.getAll().slice(0, 4));
+api.get("/programs", (req, res) => {
+  res.json([
+    "Computer Science",
+    "Computer Information Systems",
+    "Computer technology",
+  ]);
 });
 
-api.get('/projects/:id', (req, res) => {
-    res.json(projects.getById(req.params.id));
-});
-
-api.post('/projects', requireLogin, (req, res) => {
-    const {
-        name,
-        abstract,
-        authors,
-        tags
-    } = req.body;
-
-    const project = new Project(id(), name, abstract, authors, tags, req.session.uid );
-    const success = projects.save(project);
-    if (success) {
-        saveProjectsDb(projects.data);
-    }
-    handlePost(success,{},  projects.errors, res);
-});
-
-api.post('/projects/update', (req, res) => {
-    const {
-        id,
-        obj
-    } = req.body;
-
-    const success = projects.update(obj, id);
-    handlePost(success, projects.errors, res);
-});
-
-api.get('/programs', (req, res) => {
-    res.json([
-        'Computer Science',
-        'Computer Information Systems',
-        'Computer technology'
-    ]);
-});
-
-api.get('/graduationYears', (req, res) => {
-    res.json([
-        '2015',
-        '2016',
-        '2017',
-        '2018',
-        '2019',
-        '2020'
-    ]);
+api.get("/graduationYears", (req, res) => {
+  res.json(["2015", "2016", "2017", "2018", "2019", "2020"]);
 });
 
 module.exports = api;
