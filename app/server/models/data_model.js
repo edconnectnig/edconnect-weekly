39df30560e6a172372a0caae3624e74f4bf6a341
Balogun Habeeb <hballow2008@gmail.com>
2021-03-22 14:07:53 +0100

    Added initial JavaScript files to the Project Explorer Project

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -8,7 +8,11 @@ class DataModel {
     }
 
     getById(id) {
+        let user = this.data.find(obj => {
+            return obj.id === id;
+        });
 
+        return (user ? user : null);
     }
 
     save(obj) {
@@ -20,11 +24,26 @@ class DataModel {
     }
 
     update(obj, id) {
+        let user = this.data.find(item => item.id === id);
+        if (user) {
+            for (const item in obj) {
+                user[item] = obj[item];
+            }
+            return true;
+        }
+        return false;
 
     }
 
     delete(id) {
+        let user = this.data.find(item => item.id === id);
 
+        let index = this.data.indexOf(user);
+        if (user) {
+            this.data.splice(index, 1);
+            return true;
+        }
+        return false;
     }
 
     // this method will be overriden in the sub classes
--- app/server/models/projects.js
+++ app/server/models/projects.js
@@ -2,14 +2,31 @@ const DataModel = require('./data_model');
 
 class Project {
     constructor(id, name, abstract, authors, tags, createdBy) {
-
+        this.id = id;
+        this.name = name;
+        this.abstract = abstract;
+        this.authors = authors;
+        this.tags = tags;
+        this.createdBy = createdBy;
     }
 }
 
 class Projects extends DataModel {
     validate(obj) {
-
-    }
+ 
+        let sta1 = true;
+        for (const item in obj) {
+            if (!obj[item] || obj[item] === null) {
+                sta1 = false;
+            }
+        }
+        let arrayAuthors = Array.isArray(obj.authors);
+        let arrayTags = Array.isArray(obj.tags);
+        if (arrayAuthors && arrayTags && sta1) {
+            return true
+        }
+        return false;
+}
 }
 
 
