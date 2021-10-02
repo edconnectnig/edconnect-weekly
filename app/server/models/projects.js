3f6037028353b9d182c0402359ad03870f8652b8
tobi aroninuola <aroninuola.oluwatobi@gmail.com>
2021-10-02 05:34:40 +0100

    Added initial JavaScript filesto the Project Explorer Project

--- app/server/models/projects.js
+++ app/server/models/projects.js
@@ -14,18 +14,12 @@ class Project {
 class Projects extends DataModel {
 validate(obj) {
         this.errors = [];
-        for (let prop in obj) {
-            if (obj[prop].length == 0) {
-                this.errors.push(`${prop} should not be empty`)
-            } 
-        }
-        if (Array.isArray(obj.authors) === false || obj.authors === "") {
+        if (Array.isArray(obj.authors) === false) {
             this.errors.push("Authors should be an array");
-        } 
-        if (Array.isArray(obj.tags) === false || obj.tags === "") {
+        } if (Array.isArray(obj.tags) === false) {
             this.errors.push("Tags should be an array");
-        
-        } if (obj.id === "") {
+        } 
+        if (obj.id === "") {
             this.errors.push("id should not be empty");
         } if (obj.name === "") {
             this.errors.push("name should not be empty");
