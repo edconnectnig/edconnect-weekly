67c415f68bb89b28b53a5e1458c8a2c7d819f217
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-12-16 12:42:01 +0100

    Completed Week 7 Exercise

--- app/web/src/shared/Footer.jsx
+++ app/web/src/shared/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Navbar
       bg="light"
-      className="mx-auto w-75 p-2 pr-3 mt-5 justify-content-end"
+      className="mx-auto w-50 p-2 pr-3 mt-5 justify-content-end"
     >
       <Nav>
         Project Explorer &nbsp; &copy; 2021 - &nbsp; <b>Edconnect</b>
