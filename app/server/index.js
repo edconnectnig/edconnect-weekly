ce46e4ad366ddbfbf6b7f485af082a75d2f56403
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-12-23 23:43:19 +0100

    Completed week 8 exercise

--- app/server/index.js
+++ app/server/index.js
@@ -4,12 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const session = require("express-session");
+const register = require("@react-ssr/express/register");
+const flash = require("express-flash");
 const app = express();
 const SERVER_PORT = process.env.SERVER_PORT;
-const register = require("@react-ssr/express/register");
 
-(async () => {
-  await register(app);
+register(app).then(() => {
+  // move all app code here
 
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,30 +20,34 @@ const register = require("@react-ssr/express/register");
     );
     next();
   });
-})();
 
-app.use(morgan("combined"));
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+  app.use(morgan("combined"));
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
 
-app.use(
-  session({
-    secret: "secret",
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
-    resave: true,
-    saveUninitialized: false,
-  })
-);
+  app.use(
+    session({
+      secret: "secret",
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      },
+      resave: true,
+      saveUninitialized: false,
+    })
+  );
 
-app.use("/api", require("./routes/api"));
-app.use(express.static("public"));
+  app.use("/api", require("./routes/api"));
+  app.use(flash());
+  app.use("/", require("./controllers/home"));
+  app.use("/", require("./controllers/user"));
+  app.use("/", require("./controllers/project"));
+  app.use(express.static("public"));
 
-app.listen(SERVER_PORT, () =>
-  console.log("Server listening on port " + SERVER_PORT)
-);
+  app.listen(SERVER_PORT, () =>
+    console.log("Server listening on port " + SERVER_PORT)
+  );
+});
